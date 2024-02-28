3ee6fd90a76051e4a4b2230748c7507dd1995752
rjohnsonanr <robjohnson@ucanr.edu>
2024-02-28 12:31:07 -0800

    Reconfigure

--- NOW/NOW/asgi.py
+++ NOW/NOW/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for NavelOrangeWorm project.
+ASGI config for NOW project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NavelOrangeWorm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NOW.settings')
 
 application = get_asgi_application()
--- NOW/NOW/wsgi.py
+++ NOW/NOW/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for NavelOrangeWorm project.
+WSGI config for NOW project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NavelOrangeWorm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NOW.settings')
 
 application = get_wsgi_application()
