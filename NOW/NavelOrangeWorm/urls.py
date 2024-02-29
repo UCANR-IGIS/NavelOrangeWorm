c26f3fa130b5132f99641d434a7907033427b818
rjohnsonanr <robjohnson@ucanr.edu>
2024-02-29 08:55:46 -0800

    Initial content framework

--- NOW/NavelOrangeWorm/urls.py
+++ NOW/NavelOrangeWorm/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("events", views.events, name="events"),
+    path("contact", views.contact, name="contact"),
+    path("overview", views.overview, name="overview"),
+    path("phenology", views.phenology, name="phenology"),
+    path("resources", views.resources, name="resources"),
 ]
--- NOW/NavelOrangeWorm/views.py
+++ NOW/NavelOrangeWorm/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render(request, 'index.html')
 
+def events(request):
+    return render(request, 'events.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def overview(request):
+    return render(request, 'overview.html')
+
+def phenology(request):
+    return render(request, 'phenology.html')
+
+def resources(request):
+    return render(request, 'resources.html')
