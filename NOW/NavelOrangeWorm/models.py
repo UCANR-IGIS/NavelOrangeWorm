d613537323143176147a95996249a258e50fc954
rjohnsonanr <robjohnson@ucanr.edu>
2024-03-04 08:59:26 -0800

    Styling updates

--- NOW/NavelOrangeWorm/models.py
+++ NOW/NavelOrangeWorm/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=100)
+    content = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.name
